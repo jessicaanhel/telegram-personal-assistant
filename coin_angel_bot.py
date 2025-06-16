be17958af0b2492745de0398af1a7c061b6ed2b7
jessicaanhel <ro.project2020@gmail.com>
2025-06-16 22:58:46 +0200

    Create CoinAngelBot class

--- coin_angel_bot.py
+++ coin_angel_bot.py
@@ -1,28 +1,18 @@
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-from handlers.price_alert_handler import conv_handler_product_picker
-from handlers.inline_handler import inline_button_handler
-from handlers.extended_function_handler import conv_handler_extended
-from handlers.empty_function_handler import conv_handler_empty_1
-from handlers.start_handler import start_command
+from telegram.ext import Application
 from utils.constants import TELEGRAM_BOT_TOKEN
 
 class CoinAngelBot:
     def __init__(self, token=TELEGRAM_BOT_TOKEN):
         self.application = Application.builder().token(token).build()
 
-    def register_handlers(self):
-        self.application.add_handler(CommandHandler("start", start_command))
-        self.application.add_handler(conv_handler_extended)
-        self.application.add_handler(conv_handler_empty_1)
-        self.application.add_handler(conv_handler_product_picker)
-        self.application.add_handler(CallbackQueryHandler(inline_button_handler, pattern="^run_extended_function$"))
-        self.application.add_handler(CallbackQueryHandler(inline_button_handler, pattern="^empty_function_1$"))
-        self.application.add_handler(CallbackQueryHandler(inline_button_handler, pattern="^empty_function_2$"))
-        self.application.add_handler(CallbackQueryHandler(inline_button_handler))  # Catch-all fallback
+    def add_handler(self, handler):
+        self.application.add_handler(handler)
+
+    def add_error_handler(self, handler):
+        self.application.add_error_handler(handler)
 
     def run(self):
         print("Starting CoinAngel Bot...")
-        self.register_handlers()
         self.application.run_polling()
 
     async def send_message(self, user_id, text):
